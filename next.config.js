ba0335db2a772dbf9f1a7f230a9ac14b5b25c8c0
Doston <otkurovdoston69@gmail.com>
2024-01-12 11:00:31 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["misc.scdn.co", "i.scdn.co", "geo-media.beatsource.com","wxordeyfnjporlhcdbwc.supabase.co", "i1.sndcdn.com", "media.pitchfork.com", "seed-mix-image.spotifycdn.com", "tbvswgcbrilqlaxdsxii.supabase.co"]
+  }
+}
+
+module.exports = nextConfig
